bf80828abc37a59b1e9b1edd4d9e6704f1f8bf19
roligupta-05 <groli2945@gmail.com>
2026-03-12 10:31:02 +0530

    Add files via upload

---
+++ AddingOfArray_06.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class AddingOfArray_06 {
+    public static void main(String[] args) {
+        float [] numbers = {1.5f, 2.5f, 3.5f, 4.5f, 5.5f};
+        float sum = 0;
+        for( float element : numbers) {
+            sum = sum + element;
+        }
+        System.out.println("The value of sum is: " + sum);
+    }
+    
+}
