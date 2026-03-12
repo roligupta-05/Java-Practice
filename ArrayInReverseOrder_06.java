32c598ef89d1a9896116cf1932f8f86e4823af04
roligupta-05 <groli2945@gmail.com>
2026-03-12 10:31:38 +0530

    Add files via upload

---
+++ ArrayInReverseOrder_06.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class ArrayInReverseOrder_06 {
+    public static void main(String[] args) {
+        int marks[] = {98, 45, 67, 89, 90};
+    for(int i = marks.length-1;i>=0;i--) {
+        System.out.print(marks[i] + " ");
+    }
+}
+}
