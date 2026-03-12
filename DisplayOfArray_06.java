844ae05079e19d98eded25ccf6b02cd9edf36c59
roligupta-05 <groli2945@gmail.com>
2026-03-12 22:58:43 +0530

    Add files via upload

---
+++ DisplayOfArray_06.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class DisplayOfArray_06 {
+    public static void main(String[] args) {
+        int[] marks = {98, 45, 67, 89, 90};
+        for(int i =0; i<marks.length;i++) {
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
