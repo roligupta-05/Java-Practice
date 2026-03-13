a5314a5ff08a2c338af01b210ca0625f8fa52556
roligupta-05 <groli2945@gmail.com>
2026-03-13 22:53:44 +0530

    Add files via upload

---
+++ Literals_02.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Literals_02 {
+    public static void main(String[] args) {
+        int age2 = 65;
+        short age3 = 76;
+        long ageDina = 545678889;
+        char ch ='A';
+        float f1 = 445.76f;
+        double d1 = 4.66;
+        String str = "Roli";
+        System.out.println(age3);
+       
+
+    }
+    
+}
