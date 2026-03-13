1b22a34584d44144906cafdb3961bb4e6f863698
roligupta-05 <groli2945@gmail.com>
2026-03-13 22:52:32 +0530

    Add files via upload

---
+++ FactorialRecursion_07.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class FactorialRecursion_07 {
+    static int factorial(int n) {
+        if(n==0||n==1) {
+            return 1;
+        }
+    else {
+        return n*factorial(n-1);
+    }
+}
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println("The value of factorial of " + x + " is : " + factorial(x));
+        
+    }
+    
+}
