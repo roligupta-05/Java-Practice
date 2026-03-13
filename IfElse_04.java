2b532de1649e1dfcc7d04facb59b216ee6b0afc6
roligupta-05 <groli2945@gmail.com>
2026-03-13 22:52:59 +0530

    Add files via upload

---
+++ IfElse_04.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class IfElse_04 {
+    public static void main(String[] args) {
+    int a = 18 ;
+    if (a>=18) {
+        System.out.println("You can vote.") ;
+    }
+    else {
+        System.out.println("You cannot Vote.");
+    }
+    
+}
+}
+    
