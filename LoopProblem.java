9709db42c668e8f37368f6e56e605bd500798706
roligupta-05 <groli2945@gmail.com>
2026-03-14 22:48:54 +0530

    Add files via upload

---
+++ LoopProblem.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class LoopProblem {
+    public static void main(String[] args) {
+        int n = 8;
+        int sum = 0;
+        for(int i=1;i<=2;i++) {
+            sum += n*i;
+        }
+        System.out.println(sum);
+    }
+    
+}
