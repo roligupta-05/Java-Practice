a91c12a29573fe6b0e3e933595f20407a04e07df
roligupta-05 <groli2945@gmail.com>
2026-03-16 22:43:44 +0530

    Add files via upload

---
+++ MaxWealth.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class MaxWealth {
+    public static void main(String[] args) {
+    }
+    public int maxmimumWealth(int[][] accounts, int ans) {
+       
+        for(int person=0;person<accounts.length;person++) {
+            int sum = 0;
+             for(int account=0;account<accounts[person].length;account++) {
+                sum += accounts[person][account];
+        }  
+        if(sum>ans) {
+            ans = sum;
+        }  
+    }
+        return ans;
+    }
+}
