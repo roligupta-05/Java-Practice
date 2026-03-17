4ce534013303b2fb8ea05122e119c7e4452aa31d
roligupta-05 <groli2945@gmail.com>
2026-03-17 22:59:10 +0530

    Add files via upload

---
+++ SearchingIn2DArray.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class SearchingIn2DArray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {23,4,1},
+                {18,12,3,9},
+                {78,99,34,56},
+                {18,12}
+        };
+        int target = 34;
+        int [] ans = search(arr,target);
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+    static int[] search (int[][] arr, int target){
+        for (int row=0; row<arr.length; row++) {
+            for (int col=0; col<arr[row].length; col++) {
+                if (arr[row][col] == target) {
+                    return new int[] {row, col};
+                }
+            }
+        }
+        return new int[] {-1, -1};
+    }
+}
