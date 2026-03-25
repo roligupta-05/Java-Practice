9cade85dad34de2c30b9671d19d07d2c894d9e41
roligupta-05 <groli2945@gmail.com>
2026-03-25 20:28:01 +0530

    Add files via upload

---
+++ Factorial_Rrecursion.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Factorial_Rrecursion {
+
+        public static int factorial(int n) {
+            if (n == 0 || n == 1) {
+                return 1;
+            }
+            return n * factorial(n - 1);
+        }
+        public static void main(String[] args) {
+            int num = 5; 
+            System.out.println("Factorial of " + num + " is: " + factorial(num));
+        }
+    }
