1b9e561f9ab74a65caa4645734978225546c2b3c
roligupta-05 <groli2945@gmail.com>
2026-03-25 20:37:07 +0530

    Add files via upload

---
+++ SecondLargest.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {10 , 5, 8, 12, 15};
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int num : arr) {
+            if ( num > largest ) {
+                secondLargest = largest;
+                largest = num;
+            }
+            else if ( num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        System.out.println("Second Largest: " + secondLargest);
+    }
+    
+}
