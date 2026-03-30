19dd99228378523d2a13b353217aa8cfa11b9ec4
roligupta-05 <groli2945@gmail.com>
2026-03-30 23:01:41 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,40 @@
+//worst case: O(N^2)
+//Best case: O(N^2)
+//Stable = no
+//It perform well on small list.
+
+package com.company;
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 1, 3, 2};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selection (int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for(int i = start; i <= end; i++) {
+            if(arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+    
+}
