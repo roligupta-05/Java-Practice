8f4d4fa1dcb08287041419d57a6a9e5515b73b13
roligupta-05 <groli2945@gmail.com>
2026-04-09 00:00:17 +0530

    Add files via upload

---
+++ MissingNumber2.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MissingNumber2 {
+    class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+
+        return result;
+    }
+}
+    
+}
